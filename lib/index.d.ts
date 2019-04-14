c7b6e8f77b851f4a93d137a5bc4b1f6ddda9a2b0
Sebastian Lin <bctnry@outlook.com>
2019-04-15 01:16:28 +0800

    init

---
+++ lib/index.d.ts
@@ -0,0 +1,3 @@
+export declare class HelloWorld {
+    static HelloWorld(name?: string): void;
+}
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export class HelloWorld {
+    static HelloWorld(name?: string) {
+        console.log(`Hello, ${name?name:"World"}!`);
+    }
+}
